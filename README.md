6279bf40cadbc4c266a62e17e03ada47b657b2b4
Tom Whipple <tom@smartovation.com>
2012-04-12 10:20:57 -0700

    Initial commit of example project

---
+++ README.md
@@ -0,0 +1,15 @@
+Android credit card scanning example for card.io
+================================================
+
+To give it a try:
+
+1. Clone this repo.
+2. Go to https://card.io/accounts/register, sign up for a developer account and create a new app.
+3. Download the latest Android library from https://card.io/integrate/android and unzip it into this project. 
+4. In MyScanActivity.java, set the value of MY_CARDIO_APP_TOKEN to one create at https://card.io/apps/new/
+
+That's it!!
+
+Detailed documentation can be found at https://card.io/resources/javadoc/index.html
+
+Questions? Comments? Email support@card.io
---
+++ src/io/card/scanExample/MyScanActivity.java
@@ -0,0 +1,98 @@
+package io.card.scanExample;
+
+import io.card.payment.CardIOActivity;
+import io.card.payment.CreditCard;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MyScanActivity extends Activity
+{
+	// You MUST register with card.io to get an app token. Go to https://card.io/apps/new/
+    private static final String MY_CARDIO_APP_TOKEN = "MY APP TOKEN HERE";
+	
+	final String TAG = getClass().getName();
+	
+	private Button scanButton;
+	private TextView resultTextView;
+	
+	private int MY_SCAN_REQUEST_CODE = 100; // arbitrary int
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        resultTextView = (TextView)findViewById(R.id.resultTextView);
+        scanButton = (Button)findViewById(R.id.scanButton);
+    }
+    
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    	
+    	if (CardIOActivity.canReadCardWithCamera(this)) {
+    		scanButton.setText("Scan a credit card with card.io");
+    	}
+    	else {
+    		scanButton.setText("Enter credit card information");
+    	}
+    }
+    
+    public void onScanPress(View v) {
+    	Intent scanIntent = new Intent(this, CardIOActivity.class);
+    	
+    	// required for authentication with card.io
+    	scanIntent.putExtra(CardIOActivity.EXTRA_APP_TOKEN, MY_CARDIO_APP_TOKEN);
+    	
+    	// customize these values to suit your needs.
+		scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_EXPIRY, true); // default: true
+    	scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_CVV, false); // default: false
+    	scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_ZIP, false); // default: false
+    	scanIntent.putExtra(CardIOActivity.EXTRA_SUPPRESS_MANUAL_ENTRY, true); // default: false
+    	
+    	startActivityForResult(scanIntent, MY_SCAN_REQUEST_CODE);
+    }
+    
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		if (requestCode == MY_SCAN_REQUEST_CODE && resultCode == CardIOActivity.RESULT_CARD_INFO) {
+			String resultStr;
+			if (data != null && data.hasExtra(CardIOActivity.EXTRA_SCAN_RESULT)) {
+				CreditCard scanResult = data.getParcelableExtra(CardIOActivity.EXTRA_SCAN_RESULT);
+				
+				// Never log a raw card number. Avoid displaying it, but if necessary use getFormattedCardNumber()
+				resultStr = "Card Number: " + scanResult.getRedactedCardNumber() + "\n";
+				
+				// Do something with the raw number, e.g.:
+				// myService.setCardNumber( scanResult.cardNumber );
+
+				if (scanResult.isExpiryValid()) {
+					resultStr += "Expiration Date: " + scanResult.expiryMonth + "/" + scanResult.expiryYear + "\n"; 
+				}
+				
+				if (scanResult.cvv != null) { 
+					// Never log or display a CVV
+					resultStr += "CVV has " + scanResult.cvv.length() + " digits.\n";
+				}
+				
+				if (scanResult.zip != null) {
+					resultStr += "Zip: " + scanResult.zip + "\n";
+				}
+			}
+			else {
+				resultStr = "Scan was canceled.";
+			}
+			resultTextView.setText(resultStr);
+		}
+		
+	}
+}
+
